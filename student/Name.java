400bb1c4d51034aa0fbe445fa435e7a5d22682c3
SKULLxPB26 <akshitbhardwaj0012@gmail.com>
2024-10-01 18:35:16 +0530

    Add files via upload

---
+++ student/Name.java
@@ -0,0 +1,12 @@
+package student;
+
+
+
+public class Name {
+
+    public static void studentname()
+    {
+        System.out.println("my name is akshit bhardwaj");
+    }
+    
+}
---
+++ student/Rollno.java
@@ -0,0 +1,10 @@
+package student;
+
+public class Rollno {
+
+     public static void studentrollno()
+     {
+         System.out.println("my roll no is 4406");
+     } 
+    
+}
